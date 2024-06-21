82bda2aa9a43806df1db62ae6bedf3e42165b6ae
1285438215@qq.com <1286438215@qq.com>
2024-06-21 10:38:55 +0800

    Add Permission test page

--- src/router/locale/zh-CN.ts
+++ src/router/locale/zh-CN.ts
@@ -5,6 +5,10 @@ export default {
   'menu.user.info': '信息维护',
   'menu.salmon-icons': 'Salmon Icons',
   'menu.github': '我的 Github',
-  'menu.Permissions': '权限管理',
-  'menu.Permissions.front': '前端权限管理',
+  'menu.permissions': '权限管理',
+  'menu.permissions.front': '前端权限管理',
+  'menu.permissions.page': '页面权限',
+  'menu.permissions.button': '按钮权限',
+  'menu.permissions.backend': '后台权限管理',
+  'menu.permissions.testing': '权限测试页',
 }
---
+++ src/views/permissions/testing/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="px-6 pb-6">
+    <s-navs :navs="['menu.permissions']" />
+    权限测试页A
+  </main>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
