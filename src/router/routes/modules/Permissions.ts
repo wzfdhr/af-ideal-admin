74b7fd9c69773db4c241306fa644e90f8fba0b5c
1285438215@qq.com <1286438215@qq.com>
2024-06-21 10:38:55 +0800

    Add Permission test page

--- src/router/routes/modules/Permissions.ts
+++ src/router/routes/modules/Permissions.ts
@@ -2,28 +2,98 @@ import { defaultLayout } from '@/router/constants'
 import { AppRouteRecordRaw } from '../../types'
 
 const dashboardRoutes: AppRouteRecordRaw = {
-  path: '/Permissions',
+  path: '/permissions',
   name: 'permissions',
   component: defaultLayout,
-  redirect: '/Permissions/front',
+  redirect: '/permissions/front',
   meta: {
-    locale: 'menu.Permissions',
+    locale: 'menu.permissions',
     requireAuth: true,
     order: 0,
     icon: 'icon-apps',
-    hideChildrenInMenu: true,
   },
   children: [
     {
       path: 'front',
       name: 'front',
-      component: () => import('@/views/Permissions/index.vue'),
+      component: '',
       meta: {
-        locale: 'menu.Permissions.front',
+        locale: 'menu.permissions.front',
         requireAuth: true,
         roles: ['*'],
-        activeMenu: 'Permissions',
+        activeMenu: 'permissions',
       },
+      children: [
+        {
+          path: 'page',
+          name: 'page',
+          component: () => import('@/views/permissions/page/index.vue'),
+          meta: {
+            locale: 'menu.permissions.page',
+            requireAuth: true,
+            roles: ['*'],
+            activeMenu: 'permissions',
+          },
+        },
+        {
+          path: 'button',
+          name: 'button',
+          component: () => import('@/views/permissions/button/index.vue'),
+          meta: {
+            locale: 'menu.permissions.button',
+            requireAuth: true,
+            roles: ['*'],
+            activeMenu: 'permissions',
+          },
+        },
+        {
+          path: 'testing',
+          name: 'testing',
+          component: () => import('@/views/permissions/testing/index.vue'),
+          meta: {
+            locale: 'menu.permissions.testing',
+            requireAuth: true,
+            roles: ['*'],
+            activeMenu: 'permissions',
+          },
+        },
+      ],
+    },
+    {
+      path: 'backend',
+      name: 'backend',
+      component: '',
+      meta: {
+        locale: 'menu.permissions.backend',
+        requireAuth: true,
+        roles: ['*'],
+        activeMenu: 'permissions',
+      },
+      children: [
+        {
+          path: 'page',
+          name: 'page',
+          component: () => import('@/views/backendPermissions/page/index.vue'),
+          meta: {
+            locale: 'menu.permissions.page',
+            requireAuth: true,
+            roles: ['*'],
+            activeMenu: 'permissions',
+          },
+        },
+        {
+          path: 'button',
+          name: 'button',
+          component: () =>
+            import('@/views/backendPermissions/button/index.vue'),
+          meta: {
+            locale: 'menu.permissions.button',
+            requireAuth: true,
+            roles: ['*'],
+            activeMenu: 'permissions',
+          },
+        },
+      ],
     },
   ],
 }
---
+++ src/views/permissions/page/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="px-6 pb-6">
+    <s-navs :navs="['menu.permissions']" />
+    页面测试
+  </main>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
