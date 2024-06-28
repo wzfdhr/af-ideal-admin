452ee5861a2d3428f328ae518a4759282b6553b4
1285438215@qq.com <1286438215@qq.com>
2024-06-28 16:00:32 +0800

    权限

--- src/router/guards/permission.ts
+++ src/router/guards/permission.ts
@@ -11,8 +11,10 @@ const setupPermissionGuard = (router: Router) => {
     const userStore = useUserStore()
     const menuStore = useMenuStore()
     const permission = usePermission()
+    // 此方法 用于处理权限验证逻辑
     const permissionAllow = permission.hasAccessToRoute(to)
-
+    console.log(permissionAllow)
+    // 是否从服务器获取路由 如果选择从服务器获取 则调用menu中获取menu的方法
     if (menuFromServer) {
       // TODO: implement permisison logic
       if (
@@ -25,14 +27,18 @@ const setupPermissionGuard = (router: Router) => {
       const serverMenuConfig = [...menuStore.asyncMenu, ...whiteList]
 
       let exist = false
+      // 服务器端获取的菜单配置中寻找 与to.name匹配的数量 serverMenuConfig为空或者exist变为true的时候 会停
       while (serverMenuConfig.length && !exist) {
+        // 每次循环开始 serverMenuConfig中移出第一个元素 存到el
         const el = serverMenuConfig.shift()
+        // 检查 el.name 是否与目标路由 (to.name) 相匹配。如果匹配，设置 exist = true
         if (el?.name === to.name) exist = true
+        // 如果 el 元素具有子菜单 (el.children)，则将这些子菜单追加到 serverMenuConfig 数组的末尾，以便在后续迭代中检查
         if (el?.children) {
           serverMenuConfig.push(...(el.children as RouteRecordNormalized[]))
         }
       }
-
+      // 如果有空的时候 则进入 not-found 页面 告诉用户无法访问
       if (exist && permissionAllow) {
         next()
       } else {
