65d193f59b67fe3109364fa0e4b0647b17b16f94
1285438215@qq.com <1286438215@qq.com>
2024-07-01 14:39:48 +0800

    关于

---
+++ src/router/routes/modules/about.ts
@@ -0,0 +1,29 @@
+import { defaultLayout } from '@/router/constants'
+import { AppRouteRecordRaw } from '../../types'
+
+const aboutRoutes: AppRouteRecordRaw = {
+  path: '/about',
+  name: 'about',
+  component: defaultLayout,
+  meta: {
+    locale: 'menu.about',
+    requireAuth: true,
+    order: 999,
+    icon: 'icon-message',
+  },
+  children: [
+    {
+      path: 'index',
+      name: 'index',
+      component: () => import('@/views/audit/temp/temp.vue'),
+      meta: {
+        locale: 'menu.about.temp',
+        icon: 'icon-question',
+        requireAuth: true,
+        roles: ['*'],
+      },
+    },
+  ],
+}
+
+export default aboutRoutes
