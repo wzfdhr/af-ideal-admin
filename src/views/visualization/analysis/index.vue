8388112de009667b48abb2e4c6c7ce269ec58d20
1285438215@qq.com <1286438215@qq.com>
2024-09-30 09:25:57 +0800

    更新readme文件 配置以后计划安排

--- src/views/visualization/analysis/index.vue
+++ src/views/visualization/analysis/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>数据可视化</div>
+  <main class="px-6 pb-4">
+    <s-navs :navs="['menu.visualization', 'menu.visualization.analysis']" />
+
+    <div class="s-section">舆情分析</div>
+
+    <div class="s-section mt-4">内容发布比例</div>
+  </main>
 </template>
 
 <script setup lang="ts"></script>
