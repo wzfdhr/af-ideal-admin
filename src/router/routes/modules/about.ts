b082ebdf9b599ce0482751aa2b291090960b95fa
1285438215@qq.com <1286438215@qq.com>
2024-10-12 14:50:22 +0800

    安全设置

--- src/router/routes/modules/about.ts
+++ src/router/routes/modules/about.ts
@@ -4,25 +4,25 @@ import { AppRouteRecordRaw } from '../../types'
 const aboutRoutes: AppRouteRecordRaw = {
   path: '/about',
   name: 'about',
-  component: defaultLayout,
+  component: () => import('@/views/audit/temp/temp.vue'),
   meta: {
     locale: 'menu.about',
     requireAuth: true,
     order: 999,
     icon: 'icon-message',
   },
-  children: [
-    {
-      path: 'index',
-      name: 'index',
-      component: () => import('@/views/audit/temp/temp.vue'),
-      meta: {
-        locale: 'menu.about.temp',
-        icon: 'icon-question',
-        roles: ['*'],
-      },
-    },
-  ],
+  // children: [
+  //   {
+  //     path: 'index',
+  //     name: 'index',
+  //     component: () => import('@/views/audit/temp/temp.vue'),
+  //     meta: {
+  //       locale: 'menu.about.temp',
+  //       icon: 'icon-question',
+  //       roles: ['*'],
+  //     },
+  //   },
+  // ],
 }
 
 export default aboutRoutes
