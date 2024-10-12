1fe8a62f228bdff372db8055f17b549506a2ddaa
1285438215@qq.com <1286438215@qq.com>
2024-10-12 14:50:22 +0800

    安全设置

--- src/views/user/info/index.vue
+++ src/views/user/info/index.vue
@@ -11,8 +11,12 @@
         <a-tab-pane key="info" title="基本信息">
           <info-form />
         </a-tab-pane>
-        <a-tab-pane key="security" title="安全设置">123</a-tab-pane>
-        <a-tab-pane key="attachments" title="个人附件">1452</a-tab-pane>
+        <a-tab-pane key="security" title="安全设置">
+          <info-secure />
+        </a-tab-pane>
+        <a-tab-pane key="attachments" title="个人附件">
+          <info-attachment />
+        </a-tab-pane>
       </a-tabs>
     </div>
   </main>
@@ -21,4 +25,6 @@
 <script lang="ts" setup>
 import profileBanner from './widgets/profile-banner.vue'
 import infoForm from './widgets/info-form.vue'
+import infoSecure from './widgets/info-secure.vue'
+import infoAttachment from './widgets/info-attachment.vue'
 </script>
