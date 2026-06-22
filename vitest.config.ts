b1d5804b1a5d1a13beb1fb0d6788cc529277826f
1285438215@qq.com <1286438215@qq.com>
2026-06-22 15:53:23 +0800

    test: exclude e2e specs from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
@@ -12,6 +12,7 @@ export default defineConfig({
   },
   test: {
     environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'tests/e2e/**'],
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
   },
