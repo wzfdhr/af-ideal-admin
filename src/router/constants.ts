2cfbf89cc36ace09f7aaeef8c34bceabba98736a
1285438215@qq.com <1286438215@qq.com>
2026-06-22 14:24:58 +0800

    fix: use route group layout

--- src/router/constants.ts
+++ src/router/constants.ts
@@ -1,7 +1,8 @@
 export const defaultLayout = () => import('@/layout/default-layout.vue')
 export const fullPageLayout = () => import('@/layout/full-page-layout.vue')
+export const routeGroupLayout = () => import('@/layout/route-group-layout.vue')
 
 export const whiteList = [
-  { name: 'notFound', children: [] },
+  { name: 'not-found', children: [] },
   { name: 'login', children: [] },
 ]
